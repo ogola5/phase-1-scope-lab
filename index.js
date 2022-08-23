accc453156080fdcb12cbd5622cf0af31f129277
ogola5 <ogolaevance5@gmail.com>
2022-08-23 09:00:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'lili'
+
+function upperCaseCustomerName(){
+   return customerName =  customerName.toUpperCase()  
+   
+    
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+   
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+
+function changeLeastFavoriteCustomer(){
+   return leastFavoriteCustomer = 'lolo'
+}
